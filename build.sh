867ecade58caf83984feac8093b726c21c2abd1c
Liam Keegan <liam@keegan.ch>
2019-08-27 09:04:21 +0200

    bump libSBML revision to 26106

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 BUILD_DIR=${1:-"C:"}
-N=${2:-2}
+NPROCS=${2:-2}
 
-LIBSBML_REVISION="26097"
+LIBSBML_REVISION="26106"
 LIBEXPAT_VERSION="R_2_2_7"
 SYMENGINE_VERSION="v0.4.0"
 GMP_VERSION="6.1.2"
@@ -14,7 +14,7 @@ echo "LIBSBML_REVISION: ${LIBSBML_REVISION}"
 echo "LIBEXPAT_VERSION: ${LIBEXPAT_VERSION}"
 echo "SYMENGINE_VERSION: ${SYMENGINE_VERSION}"
 echo "GMP_VERSION: ${GMP_VERSION}"
-echo "N_PROCS: ${N_PROCS}"
+echo "NPROCS: ${NPROCS}"
 echo "PATH: ${PATH}"
 g++ --version
 make --version
@@ -28,7 +28,7 @@ wget https://gmplib.org/download/gmp/gmp-${GMP_VERSION}.tar.bz2
 tar xjf "gmp-${GMP_VERSION}.tar.bz2"
 cd gmp-${GMP_VERSION}
 ./configure --prefix=$BUILD_DIR/tarball/gmp --disable-shared --enable-static --disable-assembly --with-pic
-make -j$N
+make -j$NPROCS
 make check
 make install
 cd ..
@@ -41,8 +41,8 @@ git clone -b $SYMENGINE_VERSION --depth 1 https://github.com/symengine/symengine
 cd symengine
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-O2" -DCMAKE_INSTALL_PREFIX=$BUILD_DIR/tarball/symengine -DGMP_INCLUDE_DIR=$BUILD_DIR/tarball/gmp/include -DGMP_LIBRARY=$BUILD_DIR/tarball/gmp/lib/libgmp.a ..
-make -j$N
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-O2" -DCMAKE_INSTALL_PREFIX=$BUILD_DIR/tarball/symengine -DBUILD_BENCHMARKS=OFF -DGMP_INCLUDE_DIR=$BUILD_DIR/tarball/gmp/include -DGMP_LIBRARY=$BUILD_DIR/tarball/gmp/lib/libgmp.a ..
+make -j$NPROCS
 make test
 make install
 cd ../../
@@ -54,7 +54,7 @@ git checkout $LIBEXPAT_VERSION
 mkdir build
 cd build
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -fpic" -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -fpic" -DBUILD_doc=OFF -DBUILD_examples=OFF -DBUILD_shared=off -DBUILD_tests=OFF -DBUILD_tools=OFF -DCMAKE_INSTALL_PREFIX=$BUILD_DIR/expat ../expat
-make -j$N
+make -j$NPROCS
 make install
 cd ../../
 
@@ -64,8 +64,8 @@ cd libsbml-experimental
 svn log -l 1
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -fpermissive" -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -fpermissive" -DENABLE_SPATIAL=ON -DCMAKE_INSTALL_PREFIX=$BUILD_DIR/tarball/libsbml -DWITH_CPP_NAMESPACE=ON -DLIBSBML_SKIP_SHARED_LIBRARY=ON -DWITH_BZIP2=OFF -DWITH_ZLIB=OFF -DWITH_LIBXML=OFF -DWITH_EXPAT=ON -DLIBEXPAT_INCLUDE_DIR=$BUILD_DIR/expat/include -DLIBEXPAT_LIBRARY=$BUILD_DIR/expat/lib/libexpat.a ..
-make -j$N
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -fpermissive" -DENABLE_SPATIAL=ON -DCMAKE_INSTALL_PREFIX=$BUILD_DIR/tarball/libsbml -DWITH_CPP_NAMESPACE=ON -DLIBSBML_SKIP_SHARED_LIBRARY=ON -DWITH_BZIP2=OFF -DWITH_ZLIB=OFF -DWITH_SWIG=OFF -DWITH_LIBXML=OFF -DWITH_EXPAT=ON -DLIBEXPAT_INCLUDE_DIR=$BUILD_DIR/expat/include -DLIBEXPAT_LIBRARY=$BUILD_DIR/expat/lib/libexpat.a ..
+make -j$NPROCS VERBOSE=1
 make install
 
 # include libexpat.a
