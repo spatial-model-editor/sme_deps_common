a86e57f7714e1050738483b1f9a0e9ac67bb09c9
lkeegan <liam@keegan.ch>
2020-05-19 15:56:50 +0200

    use master branch of libtiff temporarily to fix mingw libm issue on windows

--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@ SYMENGINE_VERSION="v0.6.0"
 GMP_VERSION="6.1.2"
 SPDLOG_VERSION="v1.6.0"
 MUPARSER_VERSION="v2.2.6.1"
-LIBTIFF_VERSION="v4.1.0"
+LIBTIFF_VERSION="master"
+#libtiff note: we want commit bd03e1a2 which fixed libm issue with mingw, but this is not in release v4.1.0, so just use master branch until next release
 FMT_VERSION="6.2.1"
 TBB_VERSION="v2020.2"
 OSX_DEPLOYMENT_TARGET="10.12"
@@ -91,9 +92,6 @@ cd ../../
 # build static version of libTIFF
 git clone -b $LIBTIFF_VERSION --depth 1 https://gitlab.com/libtiff/libtiff.git
 cd libtiff
-# patch to remove -lm dep on windows
-wget https://gist.githubusercontent.com/1480c1/3d981dd54aad0baeed8f822bb156fb68/raw/16ed09dafca3b4b646592c0cde82b122a643ced1/0001-Don-t-use-libm-if-MINGW-due-to-conflict-with-libmsvc.patch
-git apply 0001-Don-t-use-libm-if-MINGW-due-to-conflict-with-libmsvc.patch
 mkdir cmake-build
 cd cmake-build
 cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_DEPLOYMENT_TARGET -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX=$BUILD_DIR -Djpeg=OFF -Djpeg12=OFF -Djbig=OFF -Dlzma=OFF -Dpixarlog=OFF -Dold-jpeg=OFF -Dzstd=OFF -Dmdi=OFF -Dwebp=OFF -Dzlib=OFF -DGLUT_INCLUDE_DIR=GLUT_INCLUDE_DIR-NOTFOUND -DOPENGL_INCLUDE_DIR=OPENGL_INCLUDE_DIR-NOTFOUND ..
