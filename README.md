735a94161ab5f390aac35a598a9065dcd5f5a2d8
Liam Keegan <liam@keegan.ch>
2020-07-16 11:42:43 +0200

    - migrate appveyor windows build to travis - add 32-bit windows build

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-# libsbml-static [![Build Status](https://travis-ci.org/lkeegan/libsbml-static.svg?branch=master)](https://travis-ci.org/lkeegan/libsbml-static) [![Build status](https://ci.appveyor.com/api/projects/status/tuw34pchfl4h37uf?svg=true)](https://ci.appveyor.com/project/lkeegan/libsbml-static)
+# libsbml-static [![Build Status](https://travis-ci.org/lkeegan/libsbml-static.svg?branch=master)](https://travis-ci.org/lkeegan/libsbml-static)
 
 This repo provides the following statically compiled libraries:
 
-  - [libSBML](http://sbml.org/SBML_Projects/libSBML)
-    - taken from the **experimental** branch of the svn repo
-    - compiled with the [spatial extension](https://sourceforge.net/p/sbml/code/HEAD/tree/trunk/specifications/sbml-level-3/version-1/spatial/specification/spatial-v1-sbml-l3v1-rel0.93.pdf?format=raw) enabled
+  - [libSBML](https://github.com/sbmlteam/libsbml)
+    - development branch compiled with the [spatial extension](https://sourceforge.net/p/sbml/code/HEAD/tree/trunk/specifications/sbml-level-3/version-1/spatial/specification/spatial-v1-sbml-l3v1-rel0.95.pdf?format=raw) enabled
   - [symengine](https://github.com/symengine/symengine)
-    - compiled with LLVM enabled, using static libraries from https://github.com/lkeegan/llvm-static
+    - compiled with LLVM enabled, using static libraries from <https://github.com/lkeegan/llvm-static>
   - [libexpat](https://libexpat.github.io/)
   - [gmp](https://gmplib.org)
   - [spdlog](https://github.com/gabime/spdlog)
@@ -17,26 +16,22 @@ This repo provides the following statically compiled libraries:
 
 Get the latest versions here:
 
-  - linux: [libsbml-static-linux.tgz](https://github.com/lkeegan/libsbml-static/releases/latest/download/libsbml-static-linux.tgz)
-  - osx: [libsbml-static-osx.tgz](https://github.com/lkeegan/libsbml-static/releases/latest/download/libsbml-static-osx.tgz)
-  - windows: [libsbml-static-windows.zip](https://github.com/lkeegan/libsbml-static/releases/latest/download/libsbml-static-windows.zip)
+  - linux (gcc 9 / Ubuntu 16.04): [libsbml-static-linux.tgz](https://github.com/lkeegan/libsbml-static/releases/latest/download/libsbml-static-linux.tgz)
+  - osx (Apple clang 11 / macOS 10.14): [libsbml-static-osx.tgz](https://github.com/lkeegan/libsbml-static/releases/latest/download/libsbml-static-osx.tgz)
+  - win32 (mingw-w64-i686-gcc 10): [libsbml-static-win32.tgz](https://github.com/lkeegan/libsbml-static/releases/latest/download/libsbml-static-win32.tgz)
+  - win64 (mingw-w64-x86_64-gcc 10): [libsbml-static-win64.tgz](https://github.com/lkeegan/libsbml-static/releases/latest/download/libsbml-static-win64.tgz)
 
 ## Updating this repo
 Any tagged commit will result in a github release.
 
-To make a new release, first update the version numbers in [build.sh](https://github.com/lkeegan/libsbml-static/blob/master/build.sh#L5), then commit the changes:
+To make a new release, first update the version numbers in [build.sh](https://github.com/lkeegan/libsbml-static/blob/master/build.sh#L7), then commit the changes:
 ```
 git commit -am "revision update"
 git push
 ```
-This will trigger the linux & osx [travis builds](https://travis-ci.org/lkeegan/libsbml-static) and a windows [appveyor build](https://ci.appveyor.com/project/lkeegan/libsbml-static), which will download and compile the libraries. If the builds are sucessful, tag this commit and push the tag to github:
+This will trigger the [travis builds](https://travis-ci.org/lkeegan/libsbml-static) which will compile the libraries. If the builds are sucessful, tag this commit and push the tag to github:
 ```
 git tag <tagname>
 git push origin <tagname>
 ```
 The tagged commit will trigger the builds again, but this time they will each add an archive of the resulting static libraries to the `<tagname>` release on this github repo.
-
-To see the last 5 revisions in the experimental branch of the libSBML svn repo:
-```
- svn log -l 5 https://svn.code.sf.net/p/sbml/code/branches/libsbml-experimental
-```
--- build.sh
+++ build.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
-BUILD_DIR=${1:-"C:/libs"}
-SUDOINSTALL=${2:-""}
-TBB_OPTIONS=${3:-"compiler=gcc arch=intel64 tbb_os=linux"}
-NPROCS=${4:-2}
+source source.sh
 
 LIBSBML_VERSION="development"
 LIBEXPAT_VERSION="R_2_2_9"
 SYMENGINE_VERSION="v0.6.0"
+# symengine note: -DWITH_CPP14 flag can be removed with next release
 GMP_VERSION="6.1.2"
 SPDLOG_VERSION="v1.7.0"
 MUPARSER_VERSION="v2.2.6.1"
 LIBTIFF_VERSION="master"
-#libtiff note: we want commit bd03e1a2 which fixed libm issue with mingw, but this is not in release v4.1.0, so just use master branch until next release
+#libtiff note: we want commit bd03e1a2 which fixed libm issue with mingw, 
+# but this is not in release v4.1.0, so using master branch until next release
 FMT_VERSION="7.0.1"
 TBB_VERSION="v2020.3"
-OSX_DEPLOYMENT_TARGET="10.12"
-
-# make sure we get the right mingw64 version of g++ on appveyor
-PATH=/mingw64/bin:$PATH
-
-export MACOSX_DEPLOYMENT_TARGET=${OSX_DEPLOYMENT_TARGET}
 
 echo "LIBSBML_VERSION: ${LIBSBML_VERSION}"
 echo "LIBEXPAT_VERSION: ${LIBEXPAT_VERSION}"
@@ -31,10 +24,11 @@ echo "LIBTIFF_VERSION: ${LIBTIFF_VERSION}"
 echo "FMT_VERSION: ${FMT_VERSION}"
 echo "TBB_VERSION: ${TBB_VERSION}"
 echo "TBB_OPTIONS: ${TBB_OPTIONS}"
-echo "OSX_DEPLOYMENT_TARGET: ${OSX_DEPLOYMENT_TARGET}"
 
+NPROCS=2
 echo "NPROCS: ${NPROCS}"
 echo "PATH: ${PATH}"
+echo "SUDOCMD: ${SUDOCMD}"
 
 which g++
 g++ --version
@@ -50,11 +44,11 @@ git clone -b $TBB_VERSION --depth 1 https://github.com/intel/tbb.git
 cd tbb
 time make tbb $TBB_OPTIONS stdver=c++17 extra_inc=big_iron.inc -j$NPROCS
 ls build/*_release
-$SUDOINSTALL mkdir -p $BUILD_DIR/lib
-$SUDOINSTALL cp build/*_release/*.a $BUILD_DIR/lib
-$SUDOINSTALL mkdir -p $BUILD_DIR/include
-$SUDOINSTALL cp -r include/tbb $BUILD_DIR/include/.
-ls $BUILD_DIR/*/*
+$SUDOCMD mkdir -p $INSTALL_PREFIX/lib
+$SUDOCMD cp build/*_release/*.a $INSTALL_PREFIX/lib
+$SUDOCMD mkdir -p $INSTALL_PREFIX/include
+$SUDOCMD cp -r include/tbb $INSTALL_PREFIX/include/.
+ls $INSTALL_PREFIX/*/*
 cd ../
 
 # build static version of expat xml library
@@ -62,10 +56,10 @@ git clone -b $LIBEXPAT_VERSION --depth 1 https://github.com/libexpat/libexpat.gi
 cd libexpat
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_DEPLOYMENT_TARGET -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX=$BUILD_DIR -DEXPAT_BUILD_DOCS=OFF -DEXPAT_BUILD_EXAMPLES=OFF -DEXPAT_BUILD_TOOLS=OFF -DEXPAT_SHARED_LIBS=OFF ../expat
+cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.12" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="-fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="-fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -DEXPAT_BUILD_DOCS=OFF -DEXPAT_BUILD_EXAMPLES=OFF -DEXPAT_BUILD_TOOLS=OFF -DEXPAT_SHARED_LIBS=OFF ../expat
 time make -j$NPROCS
 make test
-$SUDOINSTALL make install
+$SUDOCMD make install
 cd ../../
 
 # build static version of libSBML including spatial extension
@@ -74,19 +68,20 @@ cd libsbml
 git status
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_DEPLOYMENT_TARGET -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX=$BUILD_DIR -DENABLE_SPATIAL=ON -DWITH_CPP_NAMESPACE=ON -DLIBSBML_SKIP_SHARED_LIBRARY=ON -DWITH_BZIP2=OFF -DWITH_ZLIB=OFF -DWITH_SWIG=OFF -DWITH_LIBXML=OFF -DWITH_EXPAT=ON -DLIBEXPAT_INCLUDE_DIR=$BUILD_DIR/include -DLIBEXPAT_LIBRARY=$BUILD_DIR/lib/libexpat.a ..
+cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.12" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="-fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="-fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -DENABLE_SPATIAL=ON -DWITH_CPP_NAMESPACE=ON -DLIBSBML_SKIP_SHARED_LIBRARY=ON -DWITH_BZIP2=OFF -DWITH_ZLIB=OFF -DWITH_SWIG=OFF -DWITH_LIBXML=OFF -DWITH_EXPAT=ON -DLIBEXPAT_INCLUDE_DIR=$INSTALL_PREFIX/include -DLIBEXPAT_LIBRARY=$INSTALL_PREFIX/lib/libexpat.a ..
 time make -j$NPROCS
-$SUDOINSTALL make install
+$SUDOCMD make install
+cd ../../
 
 # build static version of fmt
 git clone -b $FMT_VERSION --depth 1 https://github.com/fmtlib/fmt.git
 cd fmt
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_DEPLOYMENT_TARGET -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX=$BUILD_DIR -DCMAKE_CXX_STANDARD=17 -DFMT_DOC=OFF ..
+cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.12" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="-fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="-fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -DCMAKE_CXX_STANDARD=17 -DFMT_DOC=OFF ..
 make -j$NPROCS
 make test
-$SUDOINSTALL make install
+$SUDOCMD make install
 cd ../../
 
 # build static version of libTIFF
@@ -94,10 +89,10 @@ git clone -b $LIBTIFF_VERSION --depth 1 https://gitlab.com/libtiff/libtiff.git
 cd libtiff
 mkdir cmake-build
 cd cmake-build
-cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_DEPLOYMENT_TARGET -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX=$BUILD_DIR -Djpeg=OFF -Djpeg12=OFF -Djbig=OFF -Dlzma=OFF -Dpixarlog=OFF -Dold-jpeg=OFF -Dzstd=OFF -Dmdi=OFF -Dwebp=OFF -Dzlib=OFF -DGLUT_INCLUDE_DIR=GLUT_INCLUDE_DIR-NOTFOUND -DOPENGL_INCLUDE_DIR=OPENGL_INCLUDE_DIR-NOTFOUND ..
+cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.12" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="-fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="-fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -Djpeg=OFF -Djpeg12=OFF -Djbig=OFF -Dlzma=OFF -Dpixarlog=OFF -Dold-jpeg=OFF -Dzstd=OFF -Dmdi=OFF -Dwebp=OFF -Dzlib=OFF -DGLUT_INCLUDE_DIR=GLUT_INCLUDE_DIR-NOTFOUND -DOPENGL_INCLUDE_DIR=OPENGL_INCLUDE_DIR-NOTFOUND ..
 make -j$NPROCS
 make test
-$SUDOINSTALL make install
+$SUDOCMD make install
 cd ../../
 
 # build static version of muparser
@@ -105,10 +100,10 @@ git clone -b $MUPARSER_VERSION --depth 1 https://github.com/beltoforion/muparser
 cd muparser
 mkdir cmake-build
 cd cmake-build
-cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_DEPLOYMENT_TARGET -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX=$BUILD_DIR -DBUILD_TESTING=ON -DENABLE_OPENMP=OFF -DENABLE_SAMPLES=OFF ..
+cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.12" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="-fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="-fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -DBUILD_TESTING=ON -DENABLE_OPENMP=OFF -DENABLE_SAMPLES=OFF ..
 make -j$NPROCS
 make test
-$SUDOINSTALL make install
+$SUDOCMD make install
 cd ../../
 
 # build static version of spdlog
@@ -116,22 +111,23 @@ git clone -b $SPDLOG_VERSION --depth 1 https://github.com/gabime/spdlog.git
 cd spdlog
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_DEPLOYMENT_TARGET -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX=$BUILD_DIR -DSPDLOG_BUILD_TESTS=ON -DSPDLOG_BUILD_EXAMPLE=OFF -DSPDLOG_FMT_EXTERNAL=ON -DSPDLOG_NO_THREAD_ID=ON -DSPDLOG_NO_ATOMIC_LEVELS=ON -DCMAKE_PREFIX_PATH=$BUILD_DIR ..
+cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.12" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="-fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="-fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -DSPDLOG_BUILD_TESTS=ON -DSPDLOG_BUILD_EXAMPLE=OFF -DSPDLOG_FMT_EXTERNAL=ON -DSPDLOG_NO_THREAD_ID=ON -DSPDLOG_NO_ATOMIC_LEVELS=ON -DCMAKE_PREFIX_PATH=$INSTALL_PREFIX ..
 make -j$NPROCS
 make test
-$SUDOINSTALL make install
+$SUDOCMD make install
 cd ../../
 
 # build static version of gmp
 # could generate optimised code for a given host, but haven't found safe settings
 # instead use safe fallback: --disable-assembly, which should always work albeit slowly
-wget https://gmplib.org/download/gmp/gmp-${GMP_VERSION}.tar.bz2
-tar xjf "gmp-${GMP_VERSION}.tar.bz2"
+wget https://gmplib.org/download/gmp/gmp-${GMP_VERSION}.tar.xz
+# workaround for msys2 (`tar xf file.tar.xz` hangs): https://github.com/msys2/MSYS2-packages/issues/1548
+xz -dc gmp-${GMP_VERSION}.tar.xz | tar -x --file=-
 cd gmp-${GMP_VERSION}
-./configure --prefix=$BUILD_DIR --disable-shared --disable-assembly --enable-static --with-pic --enable-cxx
+./configure --prefix=$INSTALL_PREFIX --disable-shared --disable-assembly --enable-static --with-pic --enable-cxx
 time make -j$NPROCS
 time make check
-$SUDOINSTALL make install
+$SUDOCMD make install
 cd ..
 
 # build static version of symengine
@@ -139,8 +135,8 @@ git clone -b $SYMENGINE_VERSION --depth 1 https://github.com/symengine/symengine
 cd symengine
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_DEPLOYMENT_TARGET -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX=$BUILD_DIR -DBUILD_BENCHMARKS=OFF -DGMP_INCLUDE_DIR=$BUILD_DIR/include -DGMP_LIBRARY=$BUILD_DIR/lib/libgmp.a -DCMAKE_PREFIX_PATH=$BUILD_DIR -DWITH_LLVM=ON -DWITH_COTIRE=OFF -DWITH_SYMENGINE_THREAD_SAFE=OFF ..
+cmake -G "Unix Makefiles" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.12" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="-fpic -fvisibility=hidden" -DCMAKE_CXX_FLAGS="-fpic -fvisibility=hidden" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -DBUILD_BENCHMARKS=OFF -DGMP_INCLUDE_DIR=$INSTALL_PREFIX/include -DGMP_LIBRARY=$INSTALL_PREFIX/lib/libgmp.a -DCMAKE_PREFIX_PATH=$INSTALL_PREFIX -DWITH_LLVM=ON -DWITH_COTIRE=OFF -DWITH_SYMENGINE_THREAD_SAFE=OFF -DWITH_CPP14=ON ..
 time make -j$NPROCS
 time make test
-$SUDOINSTALL make install
+$SUDOCMD make install
 cd ../../
