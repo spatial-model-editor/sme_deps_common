c19a23b41337c097ec4b499f9ce7eec25ee9222e
Liam Keegan <liam@keegan.ch>
2022-11-21 10:59:34 +0100

    - llvm -> 15.0.4 - qt -> 6.4.0 - spdlog -> 1.11.0   - adds support for fmt 9 - fmt -> 9.1.0 - tbb -> 2021.7.0 - catch2 -> 3.2 - cgal -> 5.5.1 - boost -> 1.80.0 - qcustomplot -> 2.1.1   - adds support for qt 6.4, remove our patches

--- build.sh
+++ build.sh
@@ -86,7 +86,6 @@ wget https://www.qcustomplot.com/release/${QCUSTOMPLOT_VERSION}/QCustomPlot-sour
 tar xf QCustomPlot-source.tar.gz
 cp qcustomplot-source/* qcustomplot/.
 cd qcustomplot
-git apply --ignore-space-change --ignore-whitespace --verbose patch-6.2.diff
 mkdir build
 cd build
 cmake -G "Unix Makefiles" .. \
