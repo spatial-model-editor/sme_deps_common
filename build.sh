1c5f0164af23c0bb58a666e6e1c99bfa5a1c45f7
Liam Keegan <liam@keegan.ch>
2025-09-30 10:29:49 +0200

    try combining required deps of qt bundled freetype lib for vtk, mac only for now as proof of concept

--- build.sh
+++ build.sh
@@ -604,6 +604,11 @@ time ninja
 ${SUDO_CMD} ninja install
 cd ../../
 
+# try to combine .a libs on macos only for now
+cp /opt/smelibs/lib/libQt6BundledFreetype.a /opt/smelibs/lib/ft.a
+libtool -static -o /opt/smelibs/lib/libCombinedFreetype.a /opt/smelibs/lib/libQt6BundledFreetype.a /opt/smelibs/lib/libQt6BundledLibpng.a /opt/smelibs/lib/libz.a
+VTK_OPTIONS="-DFREETYPE_LIBRARY_RELEASE=/opt/smelibs/lib/libCombinedFreetype.a -DFREETYPE_INCLUDE_DIR_freetype2=/opt/smelibs/include/QtFreetype -DFREETYPE_INCLUDE_DIR_ft2build=/opt/smelibs/include/QtFreetype"
+
 # build minimal static version of VTK including GUISupportQt and RenderingQt modules
 git clone -b $VTK_VERSION --depth 1 https://github.com/Kitware/VTK.git
 cd VTK
